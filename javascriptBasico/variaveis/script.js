c7bf2d33201dbfb2a25b567c34a26c28f95e8239
Wesley Martins <wesley.mlopes@castgroup.com>
2022-09-21 08:41:00 -0300

    javascript basico e melhorias renomeando as pastas

---
+++ javascriptBasico/variaveis/script.js
@@ -0,0 +1,27 @@
+var a = "Javacript - Nivel iniciante";
+
+var b;
+b = "HTML / CSS";
+
+a = "Javacript - Nivel Intermediario"
+ 
+console.log(a);
+console.log(b);
+
+//Tipos de Dados - Javascript
+/* 
+    Boolean
+    Null
+    Undefined
+    Number
+    String
+*/
+
+var numero = 36;
+console.log(numero);
+
+console.log(numero + numero);
+
+
+var escolha = true;
+console.log(escolha);
