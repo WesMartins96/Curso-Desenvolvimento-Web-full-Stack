43c56941bb51521a8154ebe95e22609ac20174f4
Wesley Martins <wesley.mlopes@castgroup.com>
2022-09-21 09:14:42 -0300

    javascript basico II

---
+++ javascriptBasico/operacoesAritmeticas/script.js
@@ -0,0 +1,37 @@
+//Operações Aritméticas
+/*
+    +  Adição
+    -  Subtração
+    *  Multiplicação
+    /  Divisão
+    %  Modulo
+    ++ Incremento
+    -- Decremento
+ */
+
+
+var a = 10;
+var b = 15;
+
+var c = a + b;
+console.log(c);
+
+var d = a * b;
+console.log(d);
+
+var e = a - b;
+console.log(e);
+
+var f = a / b;
+console.log(f);
+
+var g = a % b;
+console.log(g);
+
+var num1 = 99;
+++num1;
+console.log(num1);
+
+var num2 = 100;
+--num2;
+console.log(num2);
