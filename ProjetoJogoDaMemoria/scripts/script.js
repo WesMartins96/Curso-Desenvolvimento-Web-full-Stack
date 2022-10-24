30da982319236176cb3785fbbdd9b739da8787a2
Wesley Martins <wesley.mlopes@castgroup.com>
2022-10-24 11:25:58 -0300

    aguardando correcoes

--- ProjetoJogoDaMemoria/scripts/script.js
+++ ProjetoJogoDaMemoria/scripts/script.js
@@ -3,34 +3,18 @@ const BACK = "cardBack";
 const CARD = "card";
 const ICON = "icon";
 
-let techs = [
-    'bootstrap',
-    'css',
-    'electron',
-    'firebase',
-    'html',
-    'javascript',
-    'jquery',
-    'mongo',
-    'node',
-    'react'
-]; 
 
-let cards = null;
 
 startGame();
 
 function startGame(){
-    cards = createCardsFromTechs(techs);
-    shuffleCards(cards);
-
-    initializeCards(cards);
+    initializeCards(game.createCardsFromTechs());
 }
 
 function initializeCards(cards){
     let gameBoard = document.getElementById("gameBoard");
 
-    for(card of cards){
+    game.cards.forEach(card => {
         let cardElement = document.createElement('div');
         cardElement.id = card.id;
         cardElement.classList.add(CARD);
@@ -41,7 +25,7 @@ function initializeCards(cards){
         cardElement.addEventListener('click', flipCard);
         gameBoard.appendChild(cardElement);
 
-    }
+    })
 }
 
 function createCardContent(card, cardElement){
@@ -67,50 +51,6 @@ function createCardFace(face, card, element){
 
 }
 
-function shuffleCards(cards){
-
-    let currentIndex = cards.length;
-    let randomIndex = 0;
-
-    while(currentIndex !== 0){
-
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-
-        [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]];
-    }
-}
-
-createCardsFromTechs(techs);
-
-function createCardsFromTechs(techs){
-
-    let cards = [];
-
-    for(let tech of techs){
-        cards.push(createPairFromTech(tech));
-    }
-
-    return cards.flatMap(pair => pair);
-}
-
-function createPairFromTech(tech){
-
-    return[{
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false,
-    }, {
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false,
-        
-    }]
-}
-
-function createIdWithTech(tech){
-    return tech + parseInt(Math.random() * 1000);
-}
 
 function flipCard(){
 
