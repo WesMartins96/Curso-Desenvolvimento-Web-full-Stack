d5cd960765c9ffe4af9fd1c322494cc333be76bb
Wesley Martins <wesley.mlopes@castgroup.com>
2022-12-20 13:21:02 -0300

    comprimindo imagens com Node.js

--- node/redimensionandoImagem/app.js
+++ node/redimensionandoImagem/app.js
@@ -1,6 +1,11 @@
 //Terceiro tem que importar o sharp para poder usa-lo
-let sharp = require('sharp');
+const sharp = require('sharp');
 
+//Importar o compress
+const compress_images = require('compress-images');
+
+//Pra usar fs precisamos importar com o require
+let fs = require('fs');
 
 //Primeiro criar Pacote Cmder (npm init) assim criará um package.json
 
@@ -10,19 +15,52 @@ let width = Number(process.argv[3]);
 //Segundo entrar no site https://sharp.pixelplumbing.com/install e realizar
 //a instalação do sharp, para resize das imagens.
 //Usa-se o Cmder (npm install sharp), criando assim uma pasta node_modules
-function resize(path, width){
+function resize(inputpath, outputPath ,width){
 
-    sharp(path).resize({
+    sharp(inputpath).resize({
         width: width
-    }).toFile('./temp/output_resize.png', (err) =>{
+    }).toFile(outputPath, (err) =>{
         if(err){
             console.log(err);
         }else{
             console.log('Imagem redimensionada com sucesso!');
+            compress(outputPath, "./compressed/")
         }
     })
 }
 
 // realizando toda essa ação de resize usando Cmder (node app.js nomeDaImagem.png tamanho)
 // vemos o resultado do resize dentro da pasta temp
-resize(path, width);
+
+
+
+//Para começar a usar o compressed = Cmder (npm install compress-images --save-dev)
+// o tamanho fica comprimido, gerando um arquivo mais leve
+function compress(pathInput, outputPath) { 
+    compress_images(pathInput, outputPath, { compress_force: false, statistic: true, autoupdate: true }, false,
+                    { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+                    { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+                    { svg: { engine: "svgo", command: "--multipass" } },
+                    { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
+    function (error, completed, statistic) {
+        console.log("-------------");
+        console.log(error);
+        console.log(completed);
+        console.log(statistic);
+        console.log("-------------");
+
+
+        //após fazer o resize o ideal é apagar o aquivo dentro da pasta
+        // temp(temporario)
+        fs.unlink(pathInput, (err) => {
+            if (err) {
+                console.log(err);
+            }else{
+                console.log(pathInput, " Apagado");
+            }
+        })
+    });
+ }
+
+resize(path, './temp/output_resize.png' ,width);
+
