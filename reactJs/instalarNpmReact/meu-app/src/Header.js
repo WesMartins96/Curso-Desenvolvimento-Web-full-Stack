fae2a6fa0ffbee9cadc9776b95824bd0666e18ed
Wesley Martins <wesley.mlopes@castgroup.com>
2022-12-23 13:56:57 -0300

    header com reactJS

---
+++ reactJs/instalarNpmReact/meu-app/src/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import './App.css';
+
+function Header() {
+    return (
+        <header className="header">
+            <h1>Wesley Martins</h1>
+            <ul className="menu">
+                <li>Sobre Mim</li>
+                <li>Contato</li>
+                <li>Projetos</li>
+            </ul>
+        </header>);
+}
+
+export default Header;
