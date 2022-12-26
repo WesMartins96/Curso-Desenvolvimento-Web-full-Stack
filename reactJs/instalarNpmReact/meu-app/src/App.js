ab0674366a4ff3170da960ede4587fdd6caa886f
Wesley Martins <wesley.mlopes@castgroup.com>
2022-12-25 23:33:14 -0300

    Hooks e useEffect

--- reactJs/instalarNpmReact/meu-app/src/App.js
+++ reactJs/instalarNpmReact/meu-app/src/App.js
@@ -2,15 +2,40 @@
 import Counter from './Counter'
 
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div>
-      {/* <Header name="Loja Wes" links={["Sobre", "Comprar", "Contato", "Login"]}></Header> */}
-      <Counter></Counter>
-    </div>
-    
+
+  const [show, setShow] = useState(true);
+
+  let time = 0;
+
+  useEffect(() => {
+    let timer = setInterval(() => {
+      time++;
+      console.log(time);
+      if (time > 5) {
+        clearInterval(timer);
+        setShow(false)
+      }
+    }, 1000)
+  }, [])
+
+
+
+  if (show) {
+    return (
+      <div>
+        {/* <Header name="Loja Wes" links={["Sobre", "Comprar", "Contato", "Login"]}></Header> */}
+        <Counter count={3}></Counter>
+      </div>
+
     );
+  } else {
+    <div>
+      sem contator
+    </div>
+  }
 }
 
 export default App;
--- reactJs/instalarNpmReact/meu-app/src/Counter.js
+++ reactJs/instalarNpmReact/meu-app/src/Counter.js
@@ -1,46 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-class Counter extends React.Component {
 
-    constructor(props) {
-        super(props);
+//Usando Hooks
+function Counter(props) {
 
-        this.state = { count: 0 }
+    //Hooks são como funções globais que a utilizamos para criar estados
 
-        this.add = this.add.bind(this)
+    //sempre nomear com setNome
+    const [count, setCount] = useState(props.count)
+
+    //Diferença usando componentDidMount() e shouldComponentUpdate() com Hooks
+    //Usando useEffect()
+
+
+    useEffect(() => {
+        setCount(parseInt(localStorage.getItem("count")));
+
+        return() => {
+            console.log("Não tem mais contador");
+        }
+    }, [])
+
+    useEffect(() => {
+        document.title = count;
+        localStorage.setItem("count", count)
+    }, [count])
+
+
+    function add() {
+        setCount(count + 1)
+
     }
 
 
-    add() {
-        //this.setState({ count: this.state.count + 1 })
-        this.setState((state) => {
-            return { count: state.count + 1 }
-        }, () => {
-            localStorage.setItem("state", JSON.stringify(this.state))
-        })
-    }
-
-    // forçar se deve sofrer update no component ou nao
-    // true - o componente será atualizado
-    // false - o component não sofrerá atualizações
-    shouldComponentUpdate() {
-        return true;
-    }
-
-    // Só quando o componente for mostrado na tela, podemos usar localStorage aqui
-    componentDidMount() {
-        this.setState(JSON.parse(localStorage.getItem("state")));
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Counter: {this.state.count}</h1>
-                <button className="button-85" onClick={this.add}>add</button>
-            </div>)
-    }
-
+    return (
+        <div>
+            <h1>Counter: {count}</h1>
+            <button className="button-85" onClick={add}>add</button>
+        </div>)
 }
+
+
+
+
+// class Counter extends React.Component {
+
+//     constructor(props) {
+//         super(props);
+
+//         this.state = { count: 0 }
+
+//         this.add = this.add.bind(this)
+//     }
+
+
+//     add() {
+//         //this.setState({ count: this.state.count + 1 })
+//         this.setState((state) => {
+//             return { count: state.count + 1 }
+//         }, () => {
+//             localStorage.setItem("state", JSON.stringify(this.state))
+//         })
+//     }
+
+//     // forçar se deve sofrer update no component ou nao
+//     // true - o componente será atualizado
+//     // false - o component não sofrerá atualizações
+//     shouldComponentUpdate() {
+//         return true;
+//     }
+
+//     // Só quando o componente for mostrado na tela, podemos usar localStorage aqui
+//     componentDidMount() {
+//         this.setState(JSON.parse(localStorage.getItem("state")));
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Counter: {this.state.count}</h1>
+//                 <button className="button-85" onClick={this.add}>add</button>
+//             </div>)
+//     }
+
+// }
 
 
 
