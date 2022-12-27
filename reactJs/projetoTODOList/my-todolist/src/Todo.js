e924948f4b7c36a138a8b075f629d34d22b0f335
Wesley Martins <wesley.mlopes@castgroup.com>
2022-12-27 02:16:48 -0300

    criacao de card e implementacoes gerais + testes

--- reactJs/projetoTODOList/my-todolist/src/Todo.js
+++ reactJs/projetoTODOList/my-todolist/src/Todo.js
@@ -1,33 +1,43 @@
 import React, { useState } from "react";
 import './Todo.css';
-import List from "./List";
-import TodoForm from "./TodoForm";
-import Item from "./Item";
+import List from "./components/List";
+import TodoForm from "./components/TodoForm";
+import Item from "./components/Item";
 
 function Todo() {
-  
+
     const [items, setItems] = useState([]);
 
-    function onAddItem(text){
+    function onAddItem(text) {
 
         let item = new Item(text);
 
         setItems([...items, item])
     }
 
-    function onItemDeleted(item){
+    function onItemDeleted(item) {
 
-        let filteredItems = items.filter(it => it.id != item.id)
+        let filteredItems = items.filter(it => it.id !== item.id)
 
         setItems(filteredItems);
+    }
+
+    function onDone(item) {
+        let updateItems = items.map(it => {
+            if (it.id === item.id) {
+                it.done = !it.done;
+            }
+            return it;
+        })
+
+        setItems(updateItems)
     }
 
     return (
         <div className="container">
             <h1>Todo</h1>
             <TodoForm onAddItem={onAddItem}></TodoForm>
-
-            <List onItemDeleted={onItemDeleted} items={items}></List>
+            <List onDone={onDone} onItemDeleted={onItemDeleted} items={items}></List>
         </div>
 
     )
--- reactJs/projetoTODOList/my-todolist/src/components/Item.js
+++ reactJs/projetoTODOList/my-todolist/src/components/Item.js
@@ -1,12 +1,12 @@
 //criar a estrutura de um objeto
 
 
-class Item{
+class Item {
 
     //Variavel de classe, ela não faz parte do objeto mas sim da classe;
     static lastId = 0;
 
-    construtor(text){
+    construtor(text) {
         this.id = Item.lastId++
         this.text = text;
         this.done = false;
